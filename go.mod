7f5c60a4612812e0113ad402498fc79d31c4a5bc
thiesen <nathanpthiesen@gmail.com>
2019-01-03 19:06:31 +0100

    define dummy routes

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/thiesen/snippetbox
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+
+		return
+	}
+
+	w.Write([]byte("Hello world!"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+
+		return
+	}
+
+	fmt.Fprintf(w, "Snippet: %d", id)
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "Method not allowed", 405)
+
+		return
+	}
+
+	w.Write([]byte("Create snippet"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	log.Println("Starting server on :4000")
+
+	err := http.ListenAndServe(":4000", mux)
+
+	log.Fatal(err)
+}
