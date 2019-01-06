720b73c911566fb7ecc88eb883fcfe21067f8831
thiesen <nathanpthiesen@gmail.com>
2019-01-06 13:32:28 +0100

    add secureheaders and logrequest middlewares

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", app.home)
@@ -13,5 +13,5 @@ func (app *application) routes() *http.ServeMux {
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	return mux
+	return app.logRequest(secureHeaders(mux))
 }
