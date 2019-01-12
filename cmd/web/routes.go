ca6467f1dbcda8f23f330e179345af35031eac71
thiesen <nathanpthiesen@gmail.com>
2019-01-12 16:56:52 +0100

    require user to be authenticated to create a snippet and logout

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,11 +10,12 @@ import (
 func (app *application) routes() http.Handler {
 	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 	dynamicMiddleware := alice.New(app.session.Enable)
+	dynamicWithAuthenticationMiddleware := dynamicMiddleware.Append(app.requireAuthenticatedUser)
 
 	mux := pat.New()
 
 	mux.Get("/", dynamicMiddleware.ThenFunc(http.HandlerFunc(app.home)))
-	mux.Get("/snippets/new", dynamicMiddleware.ThenFunc(http.HandlerFunc(app.createSnippetForm)))
+	mux.Get("/snippets/new", dynamicWithAuthenticationMiddleware.ThenFunc(http.HandlerFunc(app.createSnippetForm)))
 	mux.Post("/snippets", dynamicMiddleware.ThenFunc(http.HandlerFunc(app.createSnippet)))
 	mux.Get("/snippets/:id", dynamicMiddleware.ThenFunc(http.HandlerFunc(app.showSnippet)))
 
@@ -22,7 +23,7 @@ func (app *application) routes() http.Handler {
 	mux.Post("/user/signup", dynamicMiddleware.ThenFunc(http.HandlerFunc(app.signupUser)))
 	mux.Get("/user/login", dynamicMiddleware.ThenFunc(http.HandlerFunc(app.loginUserForm)))
 	mux.Post("/user/login", dynamicMiddleware.ThenFunc(http.HandlerFunc(app.loginUser)))
-	mux.Post("/user/logout", dynamicMiddleware.ThenFunc(http.HandlerFunc(app.logoutUser)))
+	mux.Post("/user/logout", dynamicWithAuthenticationMiddleware.ThenFunc(http.HandlerFunc(app.logoutUser)))
 
 	fileServer := http.FileServer(http.Dir("./ui//static"))
 
